a7ec00bdc48301e1e14524c115587b0d6818ff7e
dnhem27 <seoungdavid.nhem@gmail.com>
2021-04-30 13:50:59 -0400

    Update adminpage for processing tickets

--- src/main/webapp/dataset.js
+++ src/main/webapp/dataset.js
@@ -1,10 +1,10 @@
 
 var dataSet = [
-    [ "20123678293", "TRAVEL", "$500", "2021-04-05 20:26:00", "Approved", "Flight ticket purchase" ],
-    [ "34767198908", "LODGING", "$1242", "2021-03-17 05:12:42", "Pending", "Hotel and Accommodations" ],
-    [ "76320123478", "FOOD", "$120", "2021-02-01 18:30:12", "Approved", "Food delivery expense" ],
-    [ "97238012392", "LODGING", "$5420", "2021-04-20 12:02:45", "Rejected", "Vacation costs in Hawaii" ],
-    [ "52973029347", "FOOD", "$175", "2021-03-07 14:23:34", "Approved", "Catering expenses for executives party" ],
+    [ "20123678293", "TRAVEL", "$500", "2021-04-05 20:26:00", "Approved", "Flight ticket purchase", "Approve/Deny" ],
+    [ "34767198908", "LODGING", "$1242", "2021-03-17 05:12:42", "Pending", "Hotel and Accommodations", "Approve/Deny" ],
+    [ "76320123478", "FOOD", "$120", "2021-02-01 18:30:12", "Approved", "Food delivery expense", "Approve/Deny" ],
+    [ "97238012392", "LODGING", "$5420", "2021-04-20 12:02:45", "Rejected", "Vacation costs in Hawaii", "Approve/Deny" ],
+    [ "52973029347", "FOOD", "$175", "2021-03-07 14:23:34", "Approved", "Catering expenses for executives party", "Approve/Deny" ],
 ];
 
 var click = document.getElementById('view_tickets');
@@ -39,7 +39,8 @@ $(document).ready(function() {
             { title: "Reimbursement Amount" },
             { title: "Date Submitted" },
             { title: "Ticket Status" },
-            { title: "Additional Description" }
+            { title: "Additional Description" },
+            { title : "For Finance Manager Only"}
         ]
     });
 });
